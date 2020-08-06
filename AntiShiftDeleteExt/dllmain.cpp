e7b16fa5c14e69030504f8a39ac3ca3b6ab4a3d5
Jemmy Wang <wangzihao12345678@126.com>
2020-08-07 01:56:14 +0800

    Remove signal sound (Attach&Detach)

--- AntiShiftDeleteExt/dllmain.cpp
+++ AntiShiftDeleteExt/dllmain.cpp
@@ -11,13 +11,13 @@ CAntiShiftDeleteExtModule _AtlModule;
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
     if (dwReason == DLL_PROCESS_ATTACH) {
-        MessageBeep(MB_ICONINFORMATION);
+        /* MessageBeep(MB_ICONINFORMATION); */
         if (!SUCCEEDED(HookFileOperation())) {
             MessageBoxW(NULL, L"Hook Failed", L"AntiShiftDelete", MB_OK | MB_ICONERROR);
         }
     }
     if (dwReason == DLL_PROCESS_DETACH) {
-        MessageBeep(MB_ICONSTOP);
+        /* MessageBeep(MB_ICONSTOP); */
         if (!SUCCEEDED(UnHookFileOperation())) {
             MessageBoxW(NULL, L"UnHook Failed", L"AntiShiftDelete", MB_OK | MB_ICONERROR);
         }
